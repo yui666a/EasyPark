54880803d8e7aa807a40229a23dd86c7b79a9c51
yui666a <haiso666@gmail.com>
2023-08-09 03:20:41 +0900

    荒いデザインだけ実装

--- easy-park/src/App.tsx
+++ easy-park/src/App.tsx
@@ -1,26 +1,36 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Stack justifyContent="space-evenly" sx={{ height: 100 + "vh" }}>
+      <Button
+        variant="contained"
+        sx={{ borderRadius: 99, background: "lime", color: "black" }}
+      >
+        開始する
+      </Button>
+
+      <Paper sx={{ m: 2 }}>
+        <Typography variant="h6">現在</Typography>
+        <Typography>¥300</Typography>
+
+        <Typography variant="h6">次の料金アップまであと</Typography>
+        <Typography>00分00秒</Typography>
+
+        <Typography>駐車開始時刻 00:00:00</Typography>
+        <Typography>経過時間 00:00:00</Typography>
+      </Paper>
+
+      <ButtonGroup variant="contained" aria-label="">
+        <Button>Stop</Button>
+        <Button>Reset</Button>
+      </ButtonGroup>
+    </Stack>
   );
-}
+};
 
 export default App;
