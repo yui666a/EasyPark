0143f89a5c42ad0ff09a77ee0138b4594d7e592b
yui666a <haiso666@gmail.com>
2023-08-12 09:19:38 +0900

    feat: 経過時間を表示

--- easy-park/src/App.tsx
+++ easy-park/src/App.tsx
@@ -3,13 +3,44 @@ import Stack from "@mui/material/Stack";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
+import { intervalToDuration, format } from "date-fns";
+import { ja } from "date-fns/locale";
+import { useState } from "react";
 
 const App = () => {
+  const [beginningTime, setBeginningTime] = useState("00:00:00"); // 駐車開始時刻
+  const [time, setTime] = useState("00:00:00"); // 経過時間
+
+  setInterval(
+    () => {
+      if (localStorage.getItem("beginningTime")) {
+        const beginningTime = new Date(localStorage.getItem("beginningTime")!);
+        const duration = intervalToDuration({
+          start: beginningTime,
+          end: new Date(),
+        });
+        console.log(duration);
+        const { days, hours, minutes, seconds } = duration;
+        setTime(`${days}日 ${hours}時間${minutes}分${seconds}秒`);
+      }
+    },
+    1000 //1秒ごとに実行
+  );
+
+  const handleClickBeginButton = () => {
+    const formattedDate = format(new Date(), "yyyy-MM-dd HH:mm:ss", {
+      locale: ja,
+    });
+    localStorage.setItem("beginningTime", formattedDate);
+    setBeginningTime(formattedDate);
+  };
+
   return (
     <Stack justifyContent="space-evenly" sx={{ height: 100 + "vh" }}>
       <Button
         variant="contained"
         sx={{ borderRadius: 99, background: "lime", color: "black" }}
+        onClick={handleClickBeginButton}
       >
         開始する
       </Button>
@@ -21,13 +52,19 @@ const App = () => {
         <Typography variant="h6">次の料金アップまであと</Typography>
         <Typography>00分00秒</Typography>
 
-        <Typography>駐車開始時刻 00:00:00</Typography>
-        <Typography>経過時間 00:00:00</Typography>
+        <Typography>駐車開始時刻 {beginningTime}</Typography>
+        <Typography>経過時間 {time}</Typography>
       </Paper>
 
       <ButtonGroup variant="contained" aria-label="">
         <Button>Stop</Button>
-        <Button>Reset</Button>
+        <Button
+          onClick={() => {
+            localStorage.removeItem("beginningTime");
+          }}
+        >
+          Reset
+        </Button>
       </ButtonGroup>
     </Stack>
   );
